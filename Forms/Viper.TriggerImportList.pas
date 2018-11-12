3c545157f341230c8381a55cbc11cf0190731a02
himselfv@gmail.com <himselfv@gmail.com>
2018-11-12 14:45:13 +0300

    TriggerImportList: Adding grayed out nodes.

--- Forms/Viper.TriggerImportList.pas
+++ Forms/Viper.TriggerImportList.pas
@@ -13,11 +13,31 @@ uses
   Viper.TriggerList;
 
 type
+  //Additional data associated with the nodes. Goes after the inherited node data.
+  TNdTriggerImportData = record
+    GrayedOut: boolean;
+  end;
+  PNdTriggerImportData = ^TNdTriggerImportData;
+
+
   TTriggerImportList = class(TTriggerList)
+    procedure TreeGetNodeDataSize(Sender: TBaseVirtualTree;
+      var NodeDataSize: Integer);
+    procedure TreeFocusChanging(Sender: TBaseVirtualTree; OldNode,
+      NewNode: PVirtualNode; OldColumn, NewColumn: TColumnIndex;
+      var Allowed: Boolean);
+    procedure TreePaintText(Sender: TBaseVirtualTree;
+      const TargetCanvas: TCanvas; Node: PVirtualNode; Column: TColumnIndex;
+      TextType: TVSTTextType);
+    procedure TreeChecking(Sender: TBaseVirtualTree; Node: PVirtualNode;
+      var NewState: TCheckState; var Allowed: Boolean);
+  protected
+    FInheritedNodeDataSize: integer;
+    function GetTriggerImportData(Node: PVirtualNode): PNdTriggerImportData; inline;
   public
     constructor Create(AOwner: TComponent); override;
     procedure Reload; override;
-    function Add(const ATrigger: TRegTriggerEntry): PVirtualNode; reintroduce;
+    function Add(const ATrigger: TRegTriggerEntry; GrayedOut: boolean = false): PVirtualNode; reintroduce;
   end;
 
 var
@@ -33,17 +53,81 @@ begin
   Self.FEntryMode := emChildEntries;
 end;
 
+procedure TTriggerImportList.TreeGetNodeDataSize(Sender: TBaseVirtualTree;
+  var NodeDataSize: Integer);
+begin
+  inherited;
+  Self.FInheritedNodeDataSize := NodeDataSize;
+  Inc(NodeDataSize, SizeOf(TNdTriggerImportData));
+end;
+
+//Retrieves additional data this module associates with the nodes. It goes after the inherited node data.
+function TTriggerImportList.GetTriggerImportData(Node: PVirtualNode): PNdTriggerImportData;
+begin
+  Result := Tree.GetNodeData(Node);
+  if Result <> nil then
+    Result := PNdTriggerImportData(NativeUint(Result) + FInheritedNodeDataSize);
+end;
+
+procedure TTriggerImportList.TreePaintText(Sender: TBaseVirtualTree;
+  const TargetCanvas: TCanvas; Node: PVirtualNode; Column: TColumnIndex;
+  TextType: TVSTTextType);
+var Data: PNdTriggerImportData;
+begin
+  inherited;
+  Data := Self.GetTriggerImportData(Node);
+  if Data.GrayedOut then
+    TargetCanvas.Font.Color := clGrayText;
+end;
+
+procedure TTriggerImportList.TreeFocusChanging(Sender: TBaseVirtualTree;
+  OldNode, NewNode: PVirtualNode; OldColumn, NewColumn: TColumnIndex;
+  var Allowed: Boolean);
+var Data: PNdTriggerImportData;
+begin
+  inherited;
+  if NewNode = nil then exit;
+  Data := Self.GetTriggerImportData(NewNode);
+  if Data.GrayedOut then
+    Allowed := false;
+end;
+
+procedure TTriggerImportList.TreeChecking(Sender: TBaseVirtualTree;
+  Node: PVirtualNode; var NewState: TCheckState; var Allowed: Boolean);
+var Data: PNdTriggerImportData;
+begin
+  inherited;
+  Data := Self.GetTriggerImportData(Node);
+  if Data.GrayedOut then
+    Allowed := (NewState = csUncheckedNormal);
+end;
+
 procedure TTriggerImportList.Reload;
 begin
   //Do nothing -- we're reloaded from the outside.
 end;
 
+resourcestring
+  sServiceNotFound = '%s - not found';
+
 //Adds a new imported trigger entry. Internal data is copied.
-function TTriggerImportList.Add(const ATrigger: TRegTriggerEntry): PVirtualNode;
+function TTriggerImportList.Add(const ATrigger: TRegTriggerEntry; GrayedOut: boolean): PVirtualNode;
+var ServiceName: string;
+  ChildNode: PVirtualNode;
 begin
-  Result := inherited Add(ATrigger.ServiceName, ATrigger.Index, ATrigger.Trigger);
+  if not GrayedOut then
+    ServiceName := ATrigger.ServiceName
+  else
+    ServiceName := Format(sServiceNotFound, [ATrigger.ServiceName]);
+  Result := inherited Add(ServiceName, ATrigger.Index, ATrigger.Trigger);
+  GetTriggerImportData(Result).GrayedOut := GrayedOut;
+  for ChildNode in Tree.ChildNodes(Result) do //multi-entry triggers may spawn child nodes
+    GetTriggerImportData(ChildNode).GrayedOut := true;
   Tree.CheckType[Result] := ctCheckBox;
-  Tree.CheckState[Result] := csCheckedNormal;
+  if GrayedOut then
+    Tree.CheckState[Result] := csUncheckedNormal
+  else
+    Tree.CheckState[Result] := csCheckedNormal;
 end;
 
 
